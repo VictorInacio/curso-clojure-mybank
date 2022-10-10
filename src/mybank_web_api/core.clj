6ec845c502dc8aef39010d8e7e4cc52fd4068fe6
Victor Inacio <victorinacio@gmail.com>
2022-10-10 22:50:22 +0200

    Aula 02 - Unit Tests

--- src/mybank_web_api/core.clj
+++ src/mybank_web_api/core.clj
@@ -3,30 +3,61 @@
             [io.pedestal.http.route :as route]
             [io.pedestal.test :as test-http]
             [io.pedestal.interceptor :as i]
+            [io.pedestal.interceptor.chain :as chain]
             [clojure.pprint :as pp])
   (:gen-class))
 
+(defn quadrado [x] (* x x))
+
 (defonce server (atom nil))
 
-(defonce contas (atom {:1 {:saldo 100}
+(defonce contas- (atom {:1 {:saldo 100}
                        :2 {:saldo 200}
                        :3 {:saldo 300}}))
 
+(defn hello [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello!"})
+
 (defn add-contas-atom [context]
-  (update context :request assoc :contas contas))
+  (update context :request assoc :contas contas-))
 
 (def contas-interceptor
   {:name  :contas-interceptor
    :enter add-contas-atom})
 
+;(def widget-finder
+;  (i/interceptor
+;    {:enter (fn [ctx]
+;              (assoc ctx :widget {:id 1 :title "foobar"} ))}))
+
+(def widget-finder
+  (i/interceptor
+    {:enter (fn [ctx]
+              (assoc ctx :widget {:id 1 :title (:title "foobar")} ))}))
+
+(def widget-renderer
+  (i/interceptor
+    {:leave (fn [ctx]
+              (if-let [widget (:widget ctx)]
+                (assoc ctx :response {:status 200
+                                      :body   (format "Widget ID %d, Title '%s'"
+                                                      (:id widget)
+                                                      (:title widget))})
+                (assoc ctx :response {:status 404 :body "Not Found"})))}))
+
 (defn get-saldo [request]
-  (let [id-conta (-> request :path-params :id keyword)]
+  (let [contas (request :contas)
+        id-conta (-> request :path-params :id keyword)]
+    (println (str "CONTAS=>=>" @contas ))
     {:status 200
      :headers {"Content-Type" "text/plain"}
      :body (id-conta @contas "conta inválida!")}))
 
 (defn make-deposit [request]
-  (let [id-conta (-> request :path-params :id keyword)
+  (let [contas (request :contas)
+        id-conta (-> request :path-params :id keyword)
         valor-deposito (-> request :body slurp parse-double)
         SIDE-EFFECT! (swap! contas (fn [m] (update-in m [id-conta :saldo] #(+ % valor-deposito))))]
     {:status 200
@@ -36,7 +67,9 @@
 
 (def routes
   (route/expand-routes
-    #{["/saldo/:id" :get get-saldo :route-name :saldo]
+    #{
+      ["/hello" :get hello :route-name :hello]
+      ["/saldo/:id" :get get-saldo :route-name :saldo]
       ["/deposito/:id" :post make-deposit :route-name :deposito]}))
 
 
@@ -56,23 +89,4 @@
 (defn start []
   (reset! server (http/start (create-server))))
 
-(defn test-request [server verb url]
-  (test-http/response-for (::http/service-fn @server) verb url))
-(defn test-post [server verb url body]
-  (test-http/response-for (::http/service-fn @server) verb url :body body))
 
-(comment
-  (start)
-  (http/stop @server)
-
-
-  (test-request server :get "/saldo/1")
-  (test-request server :get "/saldo/2")
-  (test-request server :get "/saldo/3")
-  (test-request server :get "/saldo/4")
-  (test-post server :post "/deposito/1" "199.93")
-  (test-post server :post "/deposito/4" "325.99")
-
-  ;curl http://localhost:9999/saldo/1
-  ;curl -d "199.99" -X POST http://localhost:9999/deposito/1
-  )
--- test/mybank_web_api/core_test.clj
+++ test/mybank_web_api/core_test.clj
@@ -1,25 +1,139 @@
 (ns mybank-web-api.core-test
   (:require [clojure.test :refer :all]
-            [mybank-web-api.core :refer :all]))
+            [mybank-web-api.core :refer :all]
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.test :as test-http]
+            [io.pedestal.interceptor :as i]
+            [io.pedestal.interceptor.chain :as chain]
+            [clojure.pprint :as pp]))
+
+;1 - Executar os testes em um terminal.
+;
+;2 - Adicionar casos de testes para saque. (Não pode ter saldo negativo)
+;
+;3 - Implementar pelo menos 2 melhorias nos endpoints a partir de casos de teste imaginados que falham.
 
 (deftest a-test
-  (testing "FIXED, I will not fail."
+  (testing "FAIL, I will not fail."
     (is (= 1 1))))
 
 
+(defn test-post [server verb url body]
+  (test-http/response-for (::http/service-fn @server) verb url :body body))
+
+(defn test-request [server verb url]
+  (test-http/response-for (::http/service-fn @server) verb url))
+
+(deftest ^:basic obviedades
+    (let [a 1]
+      (testing "Verificando valores estáticos!"
+        ;;Numeros simples
+        (is (= 4 (+ 2 2)))
+
+        (testing "Testes Tipos"
+          (is (instance? Long 256))
+          (is (.startsWith "abcde" "ab")))
+
+        ;; Chamadas de função
+
+        (is (= 0 (quadrado 0)))
+        (is (= 1 (quadrado 1)))
+        (is (= 1 (quadrado -1)))
+        (is (= 16 (quadrado 4))))))
+
 (deftest api-test
-  (testing "Verificar ."
-    (is (= (do (start) (test-request server :get "/greet"))
-           {:status  200,
-            :body    "Olá Pedestal!",
-            :headers {"Strict-Transport-Security"         "max-age=31536000; includeSubdomains",
-                      "X-Frame-Options"                   "DENY",
-                      "X-Content-Type-Options"            "nosniff",
-                      "X-XSS-Protection"                  "1; mode=block",
-                      "X-Download-Options"                "noopen",
-                      "X-Permitted-Cross-Domain-Policies" "none",
-                      "Content-Security-Policy"           "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;",
-                      "Content-Type"                      "text/plain"}}
+    (testing "Verificar ."
+      (is (= (let [_ (start)
+                   resp (test-request server :get "/hello")
+                   _ (http/stop @server)]
+               resp)
+             {:status  200,
+              :body    "Hello!",
+              :headers {"Strict-Transport-Security"         "max-age=31536000; includeSubdomains",
+                        "X-Frame-Options"                   "DENY",
+                        "X-Content-Type-Options"            "nosniff",
+                        "X-XSS-Protection"                  "1; mode=block",
+                        "X-Download-Options"                "noopen",
+                        "X-Permitted-Cross-Domain-Policies" "none",
+                        "Content-Security-Policy"           "object-src 'none'; script-src 'unsafe-inline' 'unsafe-eval' 'strict-dynamic' https: http:;",
+                        "Content-Type"                      "text/plain"}}))))
+(deftest api-test2
+    (testing "Verificar ."
+      (is (= (let [_ (start)
+                   resp (test-request server :get "/hello")
+                   _ (http/stop @server)]
+               (:body resp))
+             "Hello!"))))
+#_
+(deftest enter-wig
+    (let [test-fn (:enter widget-finder)]
+      (is (= {:id 1 :title "foobar"} (:widget (test-fn {:title "foobar"})))))
+    (testing "Interceptor Chains"
+      (is (= {:id 1 :title "foobar"} (:widget (chain/execute {:title "foobar"} [widget-finder]))))
+      (is (= "Widget ID 1, Title 'foobar'"
+             (get-in (chain/execute {:title "foobar"} [widget-renderer widget-finder])
+                     [:response :body])))))
 
-           ))))
 
+(deftest requests
+  (testing "Chamando request e testando o valor de body retornado"
+    (let [_ (start)
+          resp (test-request server :get "/saldo/1")
+          _ (http/stop @server)]
+      (is (= "{:saldo 100}" (:body resp))))))
+
+(deftest are-req
+  (testing "Multiple get resp"
+    (are [a b] (= (-> (test-request server :get a)
+                      :body) b)
+               "/saldo/1" "{:saldo 100}"
+               "/saldo/2" "{:saldo 200}"
+               "/saldo/3" "{:saldo 300}")))
+
+
+(comment
+  (run-tests)
+  (macroexpand '(are [x y] (= x y)
+                           2 (+ 1 1)
+                           4 (* 2 2)))
+
+  (macroexpand '(are [a b c] (= (+ a b) c)
+                           1 2 3
+                           6 6 12  ))
+
+  (macroexpand '(are [a b c] (= (* a b) c)
+                             1 2 2
+                             6 6 36  ))
+
+  (macroexpand '(are [a b] (= (-> (test-request server :get a)
+                                  :body) c)
+                           "/saldo/1" "{:saldo 100}"))
+
+  (test-request server :get "/saldo/1")
+  (run-tests)
+  (start)
+  (http/stop @server)
+
+  ;(cljs.core.match)
+
+  (test-request server :get "/saldo/1")
+  (test-request server :get "/saldo/2")
+  (test-request server :get "/saldo/3")
+  (test-request server :get "/saldo/4")
+  (test-post server :post "/deposito/1" "199.93")
+  (test-post server :post "/deposito/4" "325.99")
+
+  ;curl http://localhost:9999/saldo/1
+  ;curl -d "199.99" -X POST http://localhost:9999/deposito/1
+
+
+  (chain/execute {:title "Titulo"} [widget-finder])
+  (chain/execute {:title "Titulo"} [widget-finder widget-renderer])
+  (chain/execute {:title "Titulo"} [widget-renderer])
+  {:title    "Titulo",
+   :widget   {:id 1, :title "Titulo"},
+   :response {:status 200, :body "Widget ID 1, Title 'Titulo'"}}
+  {:title    "Titulo",
+   :response {:status 404, :body "Not Found"}}
+  )
